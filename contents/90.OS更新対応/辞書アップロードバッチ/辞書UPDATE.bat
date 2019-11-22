9f1c06599fca83fac757caedf6255d926f086009
白石　貴規 <tshiraishi@ntvit.co.jp>
2019-11-22 07:58:03 +0000

    【概要】新規 【詳細】OS更新関連ファイル
    git-tfs-id: [http://ipddv5001:8080/tfs/ntvcollection/]$/ATOK;C64253

---
+++ contents/90.OS更新対応/辞書アップロードバッチ/辞書UPDATE.bat
@@ -0,0 +1,25 @@
+@echo off
+
+pushd C:\work\辞書アップロード用フォルダ
+echo Atok23ntvbsc.dic
+atdicup.exe jisho "Atok23ntvbsc.dic" http://svcAtokDicUP:svcAtokDicUP@atok2.ntv.jp/atokdds/admin/atdicmng.cgi?type=upload
+
+echo Atok23ntvjin.dic
+atdicup.exe jisho "Atok23ntvjin.dic" http://svcAtokDicUP:svcAtokDicUP@atok2.ntv.jp/atokdds/admin/atdicmng.cgi?type=upload
+
+echo Atok23ntvmlt.dic
+atdicup.exe jisho "Atok23ntvmlt.dic" http://svcAtokDicUP:svcAtokDicUP@atok2.ntv.jp/atokdds/admin/atdicmng.cgi?type=upload
+
+echo Atok23ntvnam.dic
+atdicup.exe jisho "Atok23ntvnam.dic" http://svcAtokDicUP:svcAtokDicUP@atok2.ntv.jp/atokdds/admin/atdicmng.cgi?type=upload
+
+echo Atok23ntvpol.dic
+atdicup.exe jisho "Atok23ntvpol.dic" http://svcAtokDicUP:svcAtokDicUP@atok2.ntv.jp/atokdds/admin/atdicmng.cgi?type=upload
+
+echo Atok23ntvspo.dic
+atdicup.exe jisho "Atok23ntvspo.dic" http://svcAtokDicUP:svcAtokDicUP@atok2.ntv.jp/atokdds/admin/atdicmng.cgi?type=upload
+
+echo Atok23ntvyok.dsc
+atdicup.exe jisho "Atok23ntvyok.dsc" http://svcAtokDicUP:svcAtokDicUP@atok2.ntv.jp/atokdds/admin/atdicmng.cgi?type=upload
+
+pause
